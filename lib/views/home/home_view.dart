8027ba5882c77c3c9cf2e54e22dd2d2473a14ccd
tianchoy <tianchoy@live.com>
2026-05-07 11:52:19 +0800

    test my account

--- lib/views/home/home_view.dart
+++ lib/views/home/home_view.dart
@@ -31,34 +31,12 @@ class HomeView extends GetView<HomeController> {
                     height: 40,
                     child: GestureDetector(
                       onTap: () {
-                        Get.toNamed('/detail');
-                        // Get.snackbar(
-                        //   '当前位置',
-                        //   '经度: ${controller.longitude.toStringAsFixed(6)}, 纬度: ${controller.latitude.toStringAsFixed(6)}',
-                        //   snackPosition: SnackPosition.TOP,
-                        // );
-                      },
-                      child: const Icon(
-                        Icons.location_on,
-                        color: Colors.red,
-                        size: 40,
-                      ),
-                    ),
-                  ),
-                  Marker(
-                    point: const LatLng(
-                      39.9042,
-                      116.4074, // 北京市中心坐标
-                    ),
-                    width: 40,
-                    height: 40,
-                    child: GestureDetector(
-                      onTap: () {
                         Get.snackbar(
-                          '北京',
-                          '经度: 116.4074, 纬度: 39.9042',
+                          '当前位置',
+                          '经度: ${controller.longitude.toStringAsFixed(6)}, 纬度: ${controller.latitude.toStringAsFixed(6)}',
                           snackPosition: SnackPosition.TOP,
                         );
+                        Get.toNamed('/detail');
                       },
                       child: const Icon(
                         Icons.location_on,
