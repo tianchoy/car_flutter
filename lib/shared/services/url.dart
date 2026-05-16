a76023dceca48eee53ebe8e1d629c3966a997166
tianchoy <tianchoy@live.com>
2026-05-16 09:19:36 +0800

    update sth

--- lib/shared/services/url.dart
+++
@@ -1,8 +0,0 @@
-String baseUrl = 'https://car.zdiot.cn:18443/api';
-String mapUrl =
-    'https://wprd0{s}.is.autonavi.com/appmaptile?x={x}&y={y}&z={z}&lang=zh_cn&size=1&scl=1&style=7';
-String loginUrl = '/sys/login';
-String logoutUrl = '/sys/logout';
-String userInfoUrl = '/sys/user/info';
-String userDeviceList = '/userDevice/list';
-String messagesListUrl = '/usermessage/listForUser';
